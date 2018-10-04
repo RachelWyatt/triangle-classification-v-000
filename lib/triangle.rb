702a8c9fea802c6c5066b33775ce9341267fe658
Rachel Wyatt <rw126@evansville.edu>
2018-10-04 11:24:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,8 @@ class Triangle
   def kind
     if side_one <= 0 || side_two <= 0 || side_three <= 0
         raise TriangleError
+    elsif side_one + side_two !> side_three 
+        raise TriangleError
     elsif side_one == side_two && side_two == side_three
       :equilateral
     elsif side_one == side_two || side_two == side_three || side_one == side_three
