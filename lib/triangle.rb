0e43a8b526ce0710ca208493d34bc38b5fad8e3f
Rachel Wyatt <rw126@evansville.edu>
2018-10-04 11:22:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,9 @@ class Triangle
   end
 
   def kind
-    if side_one == side_two && side_two == side_three
+    if side_one == 0 || side_two == 0 || side_3 == 0
+        raise PartnerError
+    elsif side_one == side_two && side_two == side_three
       :equilateral
     elsif side_one == side_two || side_two == side_three || side_one == side_three
       :isosceles
